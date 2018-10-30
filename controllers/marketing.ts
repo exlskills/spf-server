4e69bef7e3e380638d56e29a02a87a4ddfd5fc56
Brady <vanderbannond@gmail.com>
2018-10-29 18:57:44 -0700

    adding image assets

--- controllers/marketing.ts
+++ controllers/marketing.ts
@@ -19,15 +19,15 @@ export async function viewMarketing(client: GqlApi, user: IUserData, locale: str
             marketing: [
               { title: "Free & Open-source content from Github",
                 subtitle: "100% of our content is free and open-source (hosted on Github)",
-                image: null
+                image: "learn-en/assets/img/marketing/_index/ico-color-opensource.svg"
               },
               { title: "Guided, intelligent process with chatbot help",
                 subtitle: "Intelligent IDE (via EXLcode) fixes mistakes in your code and helps you learn by doing",
-                image: null
+                image: "learn-en/assets/img/marketing/_index/ico-color-guided.svg"
               },
               { title: "Expert help instantly",
                 subtitle: "Need help? Ask an expert instantly and receive one-on-one help",
-                image: null
+                image: "learn-en/assets/img/marketing/_index/ico-color-validated.svg"
               },
             ]
         }
