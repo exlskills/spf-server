cab7947e50e6cee2bfec715612e45df345cece83
Sasha Varlamov <sasha@sashavarlamov.com>
2018-12-11 18:17:39 +0530

    Marketing site WIP

--- controllers/marketing-index.ts
+++ controllers/marketing-index.ts
@@ -4,29 +4,29 @@ import {IUserData} from "../lib/jwt";
 import {fetchCourseListForView} from "./courses";
 
 export async function viewMarketingIndex(client: GqlApi, user: IUserData, locale: string) : Promise<ISPFRouteResponse> {
-    const relevant = await fetchCourseListForView(client, 'relevant');
+    const relevant = (await fetchCourseListForView(client, 'relevant')).slice(0,4);
     return {
         contentTmpl: 'marketing_index',
         layout: 'marketing',
         meta: {
-            title: 'EXLskills',
-            description: 'Learn high-paying tech skills for free'
+            title: 'Learn high-paying tech skills for free',
+            description: 'The smartest way to learn high-paying tech skills. Join the thousands of professionals who use EXLskills every day! Start Learning for FREE today!'
         },
         data: {
             courses: {
                 relevant
             },
             marketing: [
-                { title: "Free & Open-source content from Github",
-                    subtitle: "100% of our content is free and open-source (hosted on Github)",
+                { title: "100% Free Courses",
+                    subtitle: "All of our courses are completely free and community-maintained!",
                     image: "https://s3-us-west-2.amazonaws.com/exlskills-mktg-assets/site-assets/_index/ico-color-opensource.svg"
                 },
-                { title: "Guided, intelligent process with chatbot help",
-                    subtitle: "Intelligent IDE (via EXLcode) fixes mistakes in your code and helps you learn by doing",
+                { title: "Live Help & Feedback",
+                    subtitle: "Learn by doing and get on-demand instruction via chat and video!",
                     image: "https://s3-us-west-2.amazonaws.com/exlskills-mktg-assets/site-assets/_index/ico-color-guided.svg"
                 },
-                { title: "Expert help instantly",
-                    subtitle: "Need help? Ask an expert instantly and receive one-on-one help",
+                { title: "Recognized Certifications",
+                    subtitle: "Our unique guided projects and certifications get you noticed by employers!",
                     image: "https://s3-us-west-2.amazonaws.com/exlskills-mktg-assets/site-assets/_index/ico-color-validated.svg"
                 },
             ]
