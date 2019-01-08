f17a2d9d0c9d6b981779c1866ed55012b70cb24d
Stan Varlamov <stan.varlamov@exlinc.com>
2019-01-08 12:37:12 +0800

    update course and dashboard msgs

--- i18n/en.ts
+++ i18n/en.ts
@@ -13,24 +13,57 @@ export const dataIntlEn = {
         marketing: {
             poweredby: 'Proudly published with',
             hundred_pct_free: "It's 100% FREE!",
+            engineers_trust: 'Engineers From These Top Companies and Universities Trust EXLskills',
+            mil_prof_hundrs_inst: '1M+ Professionals | 100+ Institutions',
+            get_started: 'Get Started!',
+            enroll_today: 'Enroll Today!',
+            enroll_now: 'Enroll Now!',
+            intro_offer: 'Introductory Offer',
+            limited_spots_remain: 'Limited Spots Remain!',
+            sales_end_label: 'Sales End',
         },
         dashboard: {
             label: 'Dashboard',
+            return_to: 'Return to dashboard',
         },
         course: {
             open_courses_label: 'Open Courses',
             label: 'Course',
+            my_courses_label: 'My Courses',
+            no_courses_q: 'No Courses?',
             overview_label: 'Overview',
             progress_label: 'Progress',
+            learn_label: 'Learn',
+            verified_lessons: 'Verified Lessons',
+            hour_label: 'Hour',
+            hours_label_p: 'Hour(s)',
+            learn_it_live: 'Learn it LIVE With EXLskills!',
+            classes_full: 'Oops! All of our Live Classes are Full!',
+            meantime_start_at_own_pace: "Don't worry... In the meantime, you can start learning at your own pace for FREE!",
+            remember_to_check_back: 'P.S. Remember to check back later as more courses become available :)',
+            view_free_course: 'View FREE Course!',
+            unit_label: 'Unit',
+            attempts_label: 'Attempts',
+            attempts_rem_today: 'Attempts Remaining Today',
+            last_attempt: 'Last Attempt',
+            weight_label: 'Weight',
+            passed_label: 'Passed',
+            grade_label: 'Grade',
+            start_course_free: 'Start a course for FREE today with EXLskills!',
         },
         digital_diplomas: {
             label: 'Digital Diplomas',
+            overview_label: 'Digital Diploma Overview',
+            no_plans: 'Oh no, there are no plans currently available!',
+            pls_check_back_soon: 'Please check back soon to see if spaces become available!',
         },
         guided_projects: {
             label: 'Guided Projects',
+            overview_label: 'Guided Project Overview',
         },
-        live_instructors: {
-            label: 'Live Instructors',
+        instructors: {
+            label: 'Instructors',
+            live_label: 'Live Instructors',
         },
         coins: {
             label: 'Coins',
@@ -41,24 +74,31 @@ export const dataIntlEn = {
             faq_label: 'Frequently Asked Questions',
             have_qs: 'Have questions?',
             reach_via: 'Feel free to reach out to us via',
-            live_chat_here: 'live chat here!'
+            live_chat_here: 'live chat here!',
+            have_more_qs: 'Have more questions?',
         },
         certificate: {
             label: 'Certificate',
             get_get: 'Get Certified, Get Employed',
             purchase: 'Purchase Certificate',
-            price: 'Price',
-            level: 'Level',
+            price_label: 'Price',
+            level_label: 'Level',
             id_verification: 'ID Verification',
             mandatory: 'Mandatory',
         },
         class: {
             live_class_label: 'Live Class',
+            location_label: 'Location',
+            loc_online_remote_av: 'Online (Remote Audio/Video)',
+            class_duration_label: 'Class Duration',
+            hrs_of_instr_label: 'Hours of Instruction',
+            session_label: 'Session',
+            sessions_label_caps: 'SESSIONS',
+            register_label: 'Register',
+            registered_see_email: "You're registered! See Email for Details",
         },
-        configuration: {
+        config: {
             label: 'Configuration',
-        },
-        mode: {
             night_mode_label: 'Night mode',
         },
         question: {
@@ -70,6 +110,10 @@ export const dataIntlEn = {
             interview_label: 'Interview',
             online_call_30: 'Online Call (~30 Minutes)',
         },
-
+        misc: {
+            na: 'N/A',
+            note_caps: 'NOTE:',
+            oops: 'Oops!',
+        },
     }
 }
